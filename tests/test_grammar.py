13ef478ad6a23db2b5e6db85028da8d4f4a1687f
KangWon LEE <kangwon@gmail.com>
2024-03-13 18:41:19 +0900

    check modue use

--- tests/test_grammar.py
+++ tests/test_grammar.py
@@ -1,8 +1,8 @@
 import ast
+import logging
 import pathlib
 
 from typing import Tuple
-
 
 import pytest
 
@@ -11,6 +11,9 @@ file_path = pathlib.Path(__file__)
 test_folder = file_path.parent.absolute()
 proj_folder = test_folder.parent.absolute()
 
+logger = logging.getLogger(__file__)
+logger.setLevel(logging.INFO)
+
 
 def py_files() -> Tuple[pathlib.Path]:
     return tuple(proj_folder.glob("*.py"))
@@ -18,10 +21,28 @@ def py_files() -> Tuple[pathlib.Path]:
 
 @pytest.mark.parametrize("py_file", py_files())
 def test_grammar(py_file:pathlib.Path):
-    
+
     code = py_file.read_text(encoding="utf-8")
-    
+
     try:
         ast.parse(code)
     except SyntaxError as e:
-        pytest.fail(f"Syntax error in file: {py_file}\n{e}")
+        pytest.fail(f"Syntax error in file: {py_file.relative_to(proj_folder)}\n{e}")
+
+
+@pytest.mark.parametrize("py_file", py_files())
+def test_module(py_file:pathlib.Path):
+
+    code = py_file.read_text(encoding="utf-8")
+
+    tree = ast.parse(code)
+
+    for node in ast.walk(tree):
+        if isinstance(node, ast.Import):
+            for alias in node.names:
+                logger.info(f"Import: {alias.name}")
+                if "numpy" == alias.name:
+                    pytest.fail(f"Import of numpy in {py_file.relative_to(proj_folder)}")
+        elif isinstance(node, ast.ImportFrom):
+            if node.module == "numpy":
+                pytest.fail(f"Import of numpy in {py_file.relative_to(proj_folder)}")
